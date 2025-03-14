6b0b48337e517826e4ccf06c083073c34e200b8e
wiwi_hhy <yoyowinnie3@gmail.com>
2025-03-14 11:08:52 +0800

    last check

--- CPP00/ex00/magaphone.cpp
+++ CPP00/ex00/magaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: hheng < hheng@student.42kl.edu.my>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/30 11:45:13 by hheng             #+#    #+#             */
-/*   Updated: 2025/03/12 10:15:44 by hheng            ###   ########.fr       */
+/*   Updated: 2025/03/14 11:01:23 by hheng            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ int	main(int ac, char **av)
         {
             for (char *p = av[i]; *p != '\0'; ++p)
             {
-                std::cout <<(char) std::toupper(*p); //can use static_cast (more safe)
+                std::cout <<(char) std::toupper(*p);
             }
         }
     }
--- CPP00/ex01/main.cpp
+++ CPP00/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: hheng < hheng@student.42kl.edu.my>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/26 11:16:49 by hheng             #+#    #+#             */
-/*   Updated: 2025/03/12 11:24:50 by hheng            ###   ########.fr       */
+/*   Updated: 2025/03/14 10:48:45 by hheng            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,6 @@
 
 /*
 getline : read characters from input until it reached the delimiter
-
 */
 
 int main() {
@@ -32,15 +31,24 @@ int main() {
             std::cout << "\nEnter index to display details: ";
             std::string indexStr;
             std::getline(std::cin, indexStr);
-            try {
-                std::stringstream ss(indexStr);
-                int index;
-                ss >> index;
-                if (ss.fail() || !ss.eof()) {
-                    throw std::invalid_argument("Invalid input");
+            
+            // Check if input is valid (contains only digits)
+            bool isValid = !indexStr.empty();
+            for (size_t i = 0; i < indexStr.length(); i++) {
+                if (!std::isdigit(indexStr[i])) {
+                    isValid = false;
+                    break;
+                }
+            }
+            
+            if (isValid) {
+                // Convert string to integer manually
+                int index = 0;
+                for (size_t i = 0; i < indexStr.length(); i++) {
+                    index = index * 10 + (indexStr[i] - '0');
                 }
                 phoneBook.displayContact(index);
-            } catch (const std::exception &e) {
+            } else {
                 std::cout << "Invalid index input." << std::endl;
             }
         } else if (command == "EXIT") {
